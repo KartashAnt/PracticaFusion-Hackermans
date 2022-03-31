be0f47c402a638016dd8355ba5adb5381fd17362
andres <andres@correo.es>
2022-03-31 21:10:20 +0200

    Version 1.0 Andres Push de la Rama con la clase Gato creada y el primer gato añadido.

--- src/paquetePrincipal/Principal.java
+++ src/paquetePrincipal/Principal.java
@@ -1,5 +1,12 @@
 package paquetePrincipal;
 
 public class Principal {
+	public static void main(String[] args) {
+		
+		
+		Gato gatoprimero = new Gato("Bola de Nieve V", "Hembra");
+		gatoprimero.mandarMensaje();
+	
+	}
 
 }
