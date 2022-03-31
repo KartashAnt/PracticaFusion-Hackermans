253126f5ddd3cbbf54db8d79d7fb59b8e6126e78
Anton Kartashov <anton.kartashov@iessampedro.org>
2022-03-31 21:06:09 +0200

    Versio 1 de Anton
    Añado el perro que canta(¿No se si si ese considera canto ?) Megadeth

--- src/paquetePrincipal/Principal.java
+++ src/paquetePrincipal/Principal.java
@@ -1,5 +1,11 @@
 package paquetePrincipal;
 
 public class Principal {
-
+	
+	public static void main(String[] args) {
+		Perro perro=new Perro("Dave Mustain", "Pastor alemán");
+		perro.mandarMensaje();
+	}
+	
+	
 }
