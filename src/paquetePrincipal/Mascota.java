5268cdd11cf2fbd2af3d610104bffa1df58bbf5b
KartashAnt <101747771+KartashAnt@users.noreply.github.com>
2022-04-07 13:05:41 +0200

    Rama mascota davis roa (#3)
    * commit de color
    
    1) Añado Hola Equipo en el mensaje
    2) Añado el atributo de color
    
    * commit nuevo atributo
    
    * commit nuevo atributo
    
    * rebase
    
    Co-authored-by: davisroa <davisroa@correo.es>
    Co-authored-by: andres <andres@correo.es>

--- src/paquetePrincipal/Mascota.java
+++ src/paquetePrincipal/Mascota.java
@@ -2,7 +2,18 @@ package paquetePrincipal;
 
 public class Mascota {
 	private String nombre;
+	private int edad;	
+	private String color;
 
+
+	public int getEdad() {
+		return edad;
+	}
+
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+	
 	public Mascota(String nombre) {
 		this.nombre = nombre;
 
@@ -15,9 +26,18 @@ public class Mascota {
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
-
-	public void MandarMensaje() {
-		System.out.println("Soy " + this.nombre + " y te voy a contar algo o no");
+	public String getColor() {
+		return color;		
 	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	public void MandarMensaje() {		
+
+		System.out.println("Hola equipo! Soy " + this.nombre + " y te voy a contar algo o no y tengo " + this.edad + ". Soy de color " +this.color);		
+	
+	}		
 
 }
